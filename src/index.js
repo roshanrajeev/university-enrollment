01255bd2f0d1b7ac8a390b2095f16ad9a6e4a829
Roshan Rajeev <roshanrajeev2001@gmail.com>
2020-09-04 17:28:55 +0530

    Minor Fix

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ const app = express()
 app.use(express.json())
 app.use(userRouter)
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/client/build"))
+if (process.env.NODE_ENV === "development") {
+  app.use(express.static(__dirname, "../client/build"))
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "../client/build/index.html"))
   })
 }
 
