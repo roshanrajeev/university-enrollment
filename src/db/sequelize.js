11e31bc6aa23ed8fff61a5d5e1b4893003ca7b75
Roshan Rajeev <roshanrajeev2001@gmail.com>
2020-09-04 12:24:03 +0530

    AWS Database Setup

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -4,7 +4,11 @@ const sequelize = new Sequelize(
   process.env.DB_DATABASE,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
-  { dialect: "mariadb" }
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mariadb",
+  }
 )
 
 module.exports = sequelize
